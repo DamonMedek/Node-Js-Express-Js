563a68a1bbaa28ee5cb1cd97da8f2b282d25b4f0
Damon medek <68669987+DamonMedek@users.noreply.github.com>
2022-04-28 17:20:06 -0400

    Update fib.js

--- fib.js
+++ fib.js
@@ -12,3 +12,14 @@ const server = http.createServer((request, response) => {
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server Running at http://${HOSTNAME}:${PORT}`);
 });
+
+var fullArray = [0, 1, 1];
+for (var i = 0; i <= 1000;) {
+  let lastArrValues = fullArray.slice(-2);
+  let i = lastArrValues[0] + lastArrValues[1];
+  fullArray.push(i);
+  if (i >= 1000) {
+    console.log(fullArray);
+    break;
+  }
+}
